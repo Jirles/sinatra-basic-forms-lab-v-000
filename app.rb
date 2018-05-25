fb1359e54ab701ceb734d121e2b30e58a664b159
Emily Jirles <emily.jirles@gmail.com>
2018-05-25 21:23:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do 
+    erb :index 
+  end 
+  
+  get "/create_puppy" do 
+    erb :create_puppy
+  end 
+  
+  post "/display_puppy" do 
+    @puppy = Puppy.new(name=params[:name], breed=params[:breed], age=params[:age])
+    
+    erb :display_puppy
+  end 
 
 end
